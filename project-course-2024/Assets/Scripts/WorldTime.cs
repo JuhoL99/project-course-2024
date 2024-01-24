f6666b4f9619204b05f21bf3ca40a3d08aab562d
JuhoL99 <juholauren99@gmail.com>
2024-01-24 12:09:43 +0200

    Added scripts
    Added some scripts for testing

---
+++ project-course-2024/Assets/Scripts/WorldTime.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldTime : MonoBehaviour
+{
+    public static WorldTime instance;
+    [SerializeField] private float currentTime;
+    [SerializeField, Range(0, 24)] private float timeOfDay;
+    [SerializeField] private int daysPassed = 0; //How many days have passed in game
+    public float timeFraction;
+
+    [Header("Length of ingame day (seconds)")]
+    [SerializeField] private int dayLength = 600; //Length of ingame day in seconds
+
+    [Header("Hours, minutes, seconds")]
+    [SerializeField] private int hour;
+    [SerializeField] private int minute;
+    [SerializeField] private int second;
+    
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void Start()
+    {
+        currentTime = dayLength / 2; // Start at noon
+    }
+    private void Update()
+    {
+        if (currentTime > dayLength)
+        {
+            currentTime = 0;
+            daysPassed++;
+        }
+        currentTime += Time.deltaTime;
+        timeFraction = currentTime / dayLength;
+        TimeConversion(timeFraction);
+        timeOfDay = Mathf.Clamp01(currentTime / dayLength) * 24;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CheckNightState();
+        }
+    }
+    private void TimeConversion(float timeFraction)
+    {
+        float secondsPassed = timeFraction * 24 * 60 * 60;
+        hour = (int)(secondsPassed / 3600);
+        minute = (int)((secondsPassed % 3600) / 60);
+        second = (int)(secondsPassed % 60);
+    }
+    public void CheckNightState()
+    {
+        Debug.Log("test");
+    }
+}
---
+++ project-course-2024/Assets/SunBehaviour.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunBehaviour : MonoBehaviour
+{
+    [SerializeField] private Light mainLight;
+    [SerializeField] private WorldTime timer;
+    [SerializeField] private Gradient lightIntensity; //gradient for light intensity based on time
+    [SerializeField] private Gradient ambientColor; //gradient for environment tint based on current time
+
+    void Start()
+    {
+        timer = GetComponent<WorldTime>();
+        mainLight = GameObject.FindWithTag("MainLight").GetComponent<Light>(); //temporary
+    }
+    void Update()
+    {
+        UpdateLight();
+
+    }
+    private void UpdateLight()
+    {
+        if (mainLight != null)
+        {
+            RotateLight();
+            ChangeLightIntensity();
+        }
+
+    }
+    private void RotateLight()
+    {
+        mainLight.transform.localRotation = Quaternion.Euler(new Vector3((timer.timeFraction * -360) - 90, 90, 90)); //rotate sunlight from east to west
+    }
+    private void ChangeLightIntensity() //temp for testing, add rotating skybox or something?
+    {
+        RenderSettings.ambientLight = ambientColor.Evaluate(timer.timeFraction);
+        mainLight.intensity = lightIntensity.Evaluate(timer.timeFraction).grayscale;
+    }
+}

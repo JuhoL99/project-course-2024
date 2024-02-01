004f6cc39134b7cc246e6840c2b084c9fb19da2d
Oliver Vesa <olpertti@gmail.com>
2024-02-01 14:48:36 +0200

    Liikkuminen nyt toimii jotenkuten hyvin
    Tweakkaan vielä

--- project-course-2024/Assets/Scripts/Movement.cs
+++ project-course-2024/Assets/Scripts/Movement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class Movement : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class Movement : MonoBehaviour
     Vector2 moveDir2;
     Vector3 moveDir3;
     public float baseMoveSpeed;
+    Vector2 input;
 
     void Start()
     {
@@ -22,6 +24,7 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
+        UpdateMoveDir();
         Move();
     }
     void Move()
@@ -31,17 +34,23 @@ public class Movement : MonoBehaviour
     }
     public void OnWalk(InputAction.CallbackContext ctx)
     {
-        Vector2 input = ctx.ReadValue<Vector2>();
+        input = ctx.ReadValue<Vector2>();
         if (input.magnitude < 0.01f)
         {
             moving = false;
             return;
         }
         moving = true;
-        float inputAngle = Mathf.Atan2(input.y, input.x)*Mathf.Rad2Deg;
+        
+    }
+    void UpdateMoveDir()
+    {
+        if (!moving) return;
+        float inputAngle = Mathf.Atan2(input.y, input.x) * Mathf.Rad2Deg;
         float camAngle = -cam.rotation.eulerAngles.y;
-        float moveAngle = (inputAngle + camAngle)%360;
-        moveDir2 = new Vector2(Mathf.Cos(moveAngle*Mathf.Deg2Rad), Mathf.Sin(moveAngle*Mathf.Deg2Rad));
-        moveDir3 = new Vector3(moveDir2.x,0f, moveDir2.y);
+        float moveAngle = (inputAngle + camAngle) % 360;
+        moveDir2 = new Vector2(Mathf.Cos(moveAngle * Mathf.Deg2Rad), Mathf.Sin(moveAngle * Mathf.Deg2Rad));
+        moveDir3 = new Vector3(moveDir2.x, 0f, moveDir2.y);
+        transform.rotation = Quaternion.Euler(0, -moveAngle + 180, 0);
     }
 }

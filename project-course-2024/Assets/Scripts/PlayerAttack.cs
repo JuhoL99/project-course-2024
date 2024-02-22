7066036835b2ba7b63a629e8e48dfb0eb4662ff4
JuhoL99 <juholauren99@gmail.com>
2024-02-22 16:21:51 +0200

    work in progress

--- project-course-2024/Assets/Scripts/PlayerAttack.cs
+++ project-course-2024/Assets/Scripts/PlayerAttack.cs
@@ -5,7 +5,14 @@ using UnityEngine.InputSystem;
 
 public class PlayerAttack : MonoBehaviour
 {
+    private int currentComboPhase = 0;
+    private float lastInputTime;
+    private float comboTimer = 1f;
     private bool isAttacking;
+    [SerializeField] private AnimationClip[] comboSteps;
+    [SerializeField] private Animator playerAnim;
+
+
     private void Update()
     {
 
@@ -17,7 +24,28 @@ public class PlayerAttack : MonoBehaviour
     }
     private void PerformAttack()
     {
-        RaycastHit hit;
+        //if new attack within combo time, advance phase
+        //Debug.Log(currentComboPhase);
+        lastInputTime = Time.time;
+        Debug.Log(lastInputTime);
+        if(Time.time - lastInputTime > comboTimer)
+        {
+            //Debug.Log(Time.time - lastInputTime);
+            currentComboPhase = 0;
+            return;
+        }
+        if(currentComboPhase < comboSteps.Length)
+        {
+            currentComboPhase++;
+        }
+        else
+        {
+            currentComboPhase = 0;
+        }
+
+
+
+        /*RaycastHit hit;
         Debug.DrawRay(transform.position, transform.forward * 2f, Color.red, 20f);
         if (Physics.Raycast(transform.position, transform.forward, out hit, 2f))
         {
@@ -29,6 +57,6 @@ public class PlayerAttack : MonoBehaviour
             {
 
             }
-        }
+        }*/
     }
 }

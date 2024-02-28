460fcfe53a70368d43732418dd5738425cb2e39d
JuhoL99 <juholauren99@gmail.com>
2024-02-29 00:59:34 +0200

    merge complete

--- project-course-2024/Assets/Scripts/InteractableDoor.cs
+++ project-course-2024/Assets/Scripts/InteractableDoor.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractableDoor : MonoBehaviour
+public class InteractableDoor : MonoBehaviour, InteractInterface
 {
     [SerializeField] private GameObject gate;
     //closed y = 2 opened y = 5
@@ -18,13 +18,18 @@ public class InteractableDoor : MonoBehaviour
         gate = transform.GetChild(0).gameObject;
         closedPos = gate.transform.localPosition;
     }
+    public void GetInteracted(GameObject player)
+    {
+        Debug.Log("interacted");
+        ToggleGate();
+    }
     void Update()
     {
         // if interacted
-        if (Input.GetKeyDown(KeyCode.F))
+        /*if (Input.GetKeyDown(KeyCode.F))
         {
             ToggleGate();
-        }
+        }*/
         if (opening)
         {
             gate.transform.localPosition += Vector3.up * openingSpeed * Time.deltaTime;
